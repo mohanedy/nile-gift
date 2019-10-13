171522c0955e9b76d829c45f7cb7f4f5f330fae4
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2019-10-14 01:09:24 +0200

    Added 4 Pharaohs

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -42,7 +42,7 @@ class _IntroScreenState extends State<IntroScreen> {
 
   void onDonePress() {
     // Do what you want
-    Navigator.push(
+    Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => HomeScreen()),
     );
